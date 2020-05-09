40a5c30f7e2cc756898fc6ccea7af49ab0ca8c79
Darren Craine <darren@agrippa.org>
2020-05-09 12:54:44 -0700

    switched to a flags for command args

--- main.go
+++ main.go
@@ -1,43 +1,57 @@
 package main
 
 import (
-  "agrippanux/gobackup/backup"
-  "github.com/joho/godotenv"
-  "log"
-  "os"
+	"agrippanux/gobackup/backup"
+	"flag"
+	"github.com/joho/godotenv"
+	"log"
+	"os"
 )
 
 func main() {
-  log.Println("gobackup started")
+	log.Println("gobackup started")
 
-  godotenv.Load()
+	if err := godotenv.Load(); err != nil {
+		log.Println("did not find a .env file, proceeding")
+	}
 
-  source := os.Getenv("SOURCE")
-  s3Bucket := os.Getenv("S3_BUCKET")
-  s3Region := os.Getenv("S3_REGION")
+	if os.Getenv("AWS_ACCESS_KEY_ID") == "" ||
+		os.Getenv("AWS_SECRET_ACCESS_KEY") == "" {
+		log.Fatalln("please set the AWS_SECRET_KEY_ID and AWS_SECRET_ACCESS_KEY env variables")
+	}
 
-  if source == "" ||
-    s3Bucket == "" ||
-    s3Region == "" {
-    log.Fatalln("missing either SOURCE, S3_BUCKET, S3_REGION env variable")
-  }
+	source := flag.String("source", "", "source to zip")
+	s3Bucket := flag.String("s3bucket", "", "s3 bucket to use")
+	s3Region := flag.String("s3region", "", "s3 region to use (example, us-west-2)")
 
-  backup, err := backup.NewBackup(source, s3Bucket, s3Region)
-  if err != nil {
-    log.Fatalln(err)
-  }
+	flag.Parse()
 
-  // start the backup process
-  if err := backup.Compress(); err != nil {
-    log.Fatalln(err)
-  }
+	if len(*source) == 0 ||
+		len(*s3Bucket) == 0 ||
+		len(*s3Region) == 0 {
+		log.Fatalln("flags source, s3bucket, and s3region required")
+	}
 
-  // clean up after ourselves
-  defer backup.Cleanup()
+	b, err := backup.NewBackup(*source, *s3Bucket, *s3Region)
+	if err != nil {
+		log.Fatalln(err)
+	}
 
-  // show to AWS
-  if err := backup.Ship(); err != nil {
-    log.Fatalln(err)
-  }
+	// start the backup process
+	if err := b.Compress(); err != nil {
+		log.Fatalln(err)
+	}
+
+	// clean up after ourselves
+	defer func() {
+		if err := b.Cleanup(); err != nil {
+			log.Fatalln(err)
+		}
+	}()
+
+	// show to AWS
+	if err := b.Ship(); err != nil {
+		log.Fatalln(err)
+	}
 
 }
