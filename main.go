c86195025f2e538844149c13a123a2b45a547b69
Darren Craine <darren@agrippa.org>
2020-05-10 11:35:12 -0700

    added ability to load an env file as a flag

--- main.go
+++ main.go
@@ -3,6 +3,7 @@ package main
 import (
 	"agrippanux/gobackup/backup"
 	"flag"
+	"fmt"
 	"github.com/joho/godotenv"
 	"log"
 	"os"
@@ -11,15 +12,7 @@ import (
 func main() {
 	log.Println("gobackup started")
 
-	if err := godotenv.Load(); err != nil {
-		log.Println("did not find a .env file, proceeding")
-	}
-
-	if os.Getenv("AWS_ACCESS_KEY_ID") == "" ||
-		os.Getenv("AWS_SECRET_ACCESS_KEY") == "" {
-		log.Fatalln("please set the AWS_SECRET_KEY_ID and AWS_SECRET_ACCESS_KEY env variables")
-	}
-
+	envFile := flag.String("env", "", "(optional) location of env file")
 	source := flag.String("source", "", "source to zip")
 	s3Bucket := flag.String("s3bucket", "", "s3 bucket to use")
 	s3Region := flag.String("s3region", "", "s3 region to use (example, us-west-2)")
@@ -32,6 +25,21 @@ func main() {
 		log.Fatalln("flags source, s3bucket, and s3region required")
 	}
 
+	if len(*envFile) > 0 {
+		if err := godotenv.Load(*envFile); err != nil {
+			log.Fatalln(fmt.Sprintf("could not find env file at %s ", *envFile))
+		}
+		log.Print("successfully loaded env file")
+	} else {
+		if err := godotenv.Load(); err != nil {
+			log.Println("did not find a .env file, proceeding")
+		}
+	}
+
+	if os.Getenv("AWS_ACCESS_KEY_ID") == "" ||
+		os.Getenv("AWS_SECRET_ACCESS_KEY") == "" {
+		log.Fatalln("please set the AWS_SECRET_KEY_ID and AWS_SECRET_ACCESS_KEY env variables")
+	}
 	b, err := backup.NewBackup(*source, *s3Bucket, *s3Region)
 	if err != nil {
 		log.Fatalln(err)
